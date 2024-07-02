faada788f3471b6a3a1416d6eb2a05cdcf2790e0
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-07-02 10:09:50 +0200

    Navigating Programmatically

--- app/(auth)/(with-auth-layout)/layout.tsx
+++ app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,15 +1,39 @@
+'use client';
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
+
+
+const navLinks = [
+  {name: 'Register', href: '/register'},
+  {name: 'Login', href: '/login'},
+  {name: 'Just Page', href: '/justpage'}
+]
+
 
 function AuthLayout({children}: {
     children: React.ReactNode
 }) {
+
+  const pathName = usePathname();
+
   return (
     <div>     
+      {navLinks.map((link) => {
+        const isActive = pathName.startsWith(link.href)
+
+
+        return(
+          <Link href={link.href} key={link.name}>
+            {link.name}
+          </Link>
+        )
+      })}
       
         
-            <h2>Inner layout</h2>
-            {children}    
-    
+
+            {children}        
     </div>
     
   )
